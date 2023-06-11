eb25b119e87c71b0586d80d9956f86057c61ef33
ramdhadage <ram.dhadage123@gmail.com>
2023-06-11 23:56:50 +0530

    added picker input instead of shiny dropdown

--- R/helper.R
+++ R/helper.R
@@ -7,7 +7,14 @@ make_ui <- function(x, id, var) {
     sliderInput(id, var, min = rng[1], max = rng[2], value = rng)
   } else if (is.factor(x)) {
     levs <- levels(x)
-    selectInput(id, var, choices = levs, selected = levs, multiple = TRUE)
+    # selectInput(id, var, choices = levs, selected = levs, multiple = TRUE)
+    shinyWidgets::pickerInput(
+      inputId = id,
+      label = var,
+      choices = levs,
+      selected =levs,
+      multiple = TRUE
+    )
   } else {
     # Not supported
     NULL
